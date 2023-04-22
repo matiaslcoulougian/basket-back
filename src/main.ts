f241b8daf11f5e048f162b7db6b167d16400aadd
matiaslcoulougian <matias.coulougian@ing.austral.edu.ar>
2023-04-22 10:15:38 -0300

    delete api global prefix

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 8080;
-  app.setGlobalPrefix('api');
   await app.listen(port);
   console.log(`🚀 Application started on localhost ${port} 🚀`);
 }
